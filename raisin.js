3c2716e89d43f57c8fcac63c95373debcbcbda38
eze1er <papaezechiel@gmail.com>
2021-12-23 22:14:36 +0000

    first commit

---
+++ raisin.js
@@ -0,0 +1,13 @@
+const raisinAlarm = function(cookie) {
+  // Put your solution here
+  for (const element of cookie) {
+    if (element === "🍇") {
+      return "Raisin alert!";
+    }
+  }
+  return "All good!";
+};
+
+console.log(raisinAlarm(["🍫", "🍫", "🍇", "🍫"]));
+console.log(raisinAlarm(["🍫", "🍇", "🍫", "🍫", "🍇"]));
+console.log(raisinAlarm(["🍫", "🍫", "🍫"]));
